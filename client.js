3d5a5696e2c3d8c2d229929c0225063e08ab18e3
Nick Boyd <nickjohnboyd@gmail.com>
2020-02-17 08:00:40 -0700

    command/chat server finished

--- client.js
+++ client.js
@@ -3,23 +3,37 @@ const net = require('net');
 let client = net.createConnection({port: 5000}, () => {
 	console.log('\n\n--------------------------------------');
 	console.log('Welcome to the server!');
-	console.log('--------------------------------------\n\n');
+	console.log('--------------------------------------\n\n\n');
+
+	console.log('-------------- COMMANDS --------------\n');
+	console.log('"<message>" - send message to server\n');
+	console.log('"/w <receivingUser> <message>" - send message only to specific user\n');
+	console.log('"/username <newUsername>" - change username\n');
+	console.log('"/kick <kickedUser> <adminPassword>" - kick specified user from server using admin password\n');
+	console.log('"/clientlist" - list all clients on server\n');
+	console.log('"/quit" - leave server\n');
+	console.log('--------------------------------------\n\n\n');
 
 	process.stdin.on('data', data => {
 		console.log();
 		let input = data.toString().trim();
-		if(input.toLowerCase() === 'quit') {
+
+		if(input.toLowerCase() === '/quit') {
 			console.log('--- Thanks for being on the server! Come again! ---\n');
 			client.write('disconnected');
 			process.exit();
-		} else {
-			client.write(input);
-		}
+		} 
+		else client.write(input);
 	});
 });
 
 client.on('data', data => {
-	console.log(data.toString());
+	if(data.toString() === 'kick') {
+		console.log('--- You have been kicked from the server! ---\n');
+		client.write('kicked');
+		process.exit();
+	}
+	else console.log(data.toString());
 });
 
 client.on('end', (err) => {
--- server.js
+++ server.js
@@ -4,18 +4,20 @@ const fs = require('fs');
 let socketList = [];
 let id = 0;
 let chat;
+let logVar = true;
 
 const server = net.createServer(socket => {
 	socket.id = id++;
+	socket.username = `User${socket.id}`;
 	socketList.push(socket); 
 
 	console.log('--------------------------------------');
-	console.log(`Welcome user ${socket.id} to the server!`);
+	console.log(`Welcome ${socket.username} to the server!`);
 	console.log('--------------------------------------\n');
 
 	socketList.forEach(s => {
 		if(s.id !== socket.id || socket.id === 0) {
-			chat = `----- User ${socket.id} has joined the server! ------\n`;
+			chat = `----- ${socket.username} has joined the server! -----\n`;
 		}
 		if(s.id !== socket.id) s.write(chat);
 	});
@@ -23,24 +25,42 @@ const server = net.createServer(socket => {
 	chatLog(chat);
 
 	socket.on('data', data => {
-		if(data.toString() === 'disconnected') {
-			chat = `----- User ${socket.id} has disconnected from the server! -----\n`;
+		let input = data.toString();
+		
+		if(input === 'disconnected') {
+			chat = `----- ${socket.username} has disconnected from the server! -----\n`;
+			removeUser(socket);
+		}
+		else if(input === 'kicked') {
+			chat = `----- ${socket.username} has been kicked from the server! -----\n`;
+			chatLog(chat);
+			console.log(chat);
+			removeUser(socket);
+		}
+		else if(input.startsWith('/')) {
+			let inArr = input.split(' ');
+
+			if(inArr[0] === '/w') whisper(socket, inArr);
+			else if(inArr[0] === '/username') resetUsername(socket, inArr);
+			else if(inArr[0] === '/kick') kickUser(socket, inArr);
+			else if(inArr[0] === '/clientlist') listClients(socket);
+			else {
+				chat = 'Error: Incorrect command. Try again.\n';
+				socket.write(chat);
+			}
+		}
+		else {
+			chat = `${socket.username}: ${input}\n`;
 			socketList.forEach(s => {
-				if(s.id === socket.id) {
-					socketList.splice(socket.id, 1);
+				if(s.id !== socket.id) {
+					s.write(chat);
 				}
 			});
 		}
-		else chat = `User ${socket.id}: ${data.toString()}\n`;
-
-		chatLog(chat);
-		console.log(chat);
-
-		socketList.forEach(s => {
-			if(s.id !== socket.id) {
-				s.write(chat);
-			}
-		});
+		if(logVar) {
+			chatLog(chat);
+			console.log(chat);
+		}	
 	});
 });
 
@@ -59,8 +79,86 @@ server.on('end', (err) => {
 	else console.log('Server disconnected');
 });
 
+
+
+function listClients(socket) {
+	chat = '--- Client List ---\n';
+	socketList.forEach(s => {
+		chat += `${s.username}\n`;
+	});
+	socket.write(chat);
+}
+
+function kickUser(socket, inArr) {
+	let adminPassword = 'supersecretpass';
+	if(inArr[2] === adminPassword) {
+		socketList.forEach(s => {
+			if(s.username === inArr[1]) {
+				s.write('kick');
+				logVar = false;
+			}
+		});
+	}
+	else {
+		let chat = 'Error: Incorrect admin password\n';
+		socket.write(chat);
+	}
+}
+
+function resetUsername(socket, inArr) {
+	let nameTaken = false;
+	socketList.forEach(s => {
+		if(inArr[1] === s.username) {
+			nameTaken = true;
+		}
+	});
+	if(inArr[1] === socket.username) {
+		chat = 'Error: New username can\'t be the same as current one\n';
+		socket.write(chat);
+	}
+	else if (nameTaken) {
+		chat = 'Error: Username already taken by another user\n';
+		socket.write(chat);
+	}
+	else {
+		chat = `----- ${socket.username} reset username to ${inArr[1]} -----\n`;
+		socket.username = inArr[1];
+		socket.write(`--- Username is now ${socket.username} ---\n`);
+
+		socketList.forEach(s => {
+			if(s.id !== socket.id) {
+				s.write(chat);
+			}
+		});					
+	}
+}
+
+function whisper(socket, inArr) {
+	let username = inArr[1];
+	chat = `${socket.username} (/w): `;
+
+	for(let i = 2; i < inArr.length; i++) {
+		chat += inArr[i];
+		chat += ' ';
+	}
+	chat += '\n';
+	socketList.forEach(s => {
+		if(username == s.username) {
+			s.write(chat);
+		}
+	});
+}
+
+function removeUser(socket) {
+	socketList.forEach(s => {
+		if(s.id === socket.id) {
+			socketList.splice(socket.id, 1);
+		}
+	});
+}
+
 function chatLog(chat) {
 	fs.appendFile('./chat.log', `${chat}\n`, (err) => {
 		if(err) console.log(err);
 	});
-};
+}
